c5279f35a2a78397d53b2be9c02bb94566bb199c
Nicolas Lehuen <nicolas@lehuen.com>
2007-10-24 18:20:40 +0000

    Initial skeleton.

---
+++ include/ctst.h
@@ -0,0 +1,17 @@
+/*
+  ctst - (c) 2007 Nicolas Lehuen
+  This is released under the LGPL - see http://www.gnu.org/licenses/lgpl.html
+
+  $Id: $
+
+  This file contains general definitions
+*/
+#ifndef __CTST_CTST_H__
+#define __CTST_CTST_H__
+
+typedef unsigned int ctst_data;
+typedef unsigned int ctst_node_ref;
+
+#include "storage.h"
+
+#endif
---
+++ include/storage.h
@@ -0,0 +1,16 @@
+/*
+  ctst - (c) 2007 Nicolas Lehuen
+  This is released under the LGPL - see http://www.gnu.org/licenses/lgpl.html
+
+  $Id: $
+
+  This file defines the storage interface.
+*/
+#ifndef __CTST_STORAGE_H__
+#define __CTST_STORAGE_H__
+
+/* Memory allocation */
+ctst_node_ref ctst_storage_alloc(ctst_data data, ctst_node_ref next, ctst_node_ref left, ctst_node_ref right, unsigned char [] bytes, int bytes_index, int bytes_length);
+void ctst_storage_free(ctst_node_ref node);
+
+#endif
