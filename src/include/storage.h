1c7a908bb048b9c6c6bc11fe9888ee6582c0b55f
Nicolas Lehuen <nicolas@lehuen.com>
2007-10-24 19:24:25 +0000

    Definition of the whole storage interface.

--- src/include/storage.h
+++ src/include/storage.h
@@ -9,8 +9,42 @@
 #ifndef __CTST_STORAGE_H__
 #define __CTST_STORAGE_H__
 
-/* Memory allocation */
-ctst_node_ref ctst_storage_alloc(ctst_data data, ctst_node_ref next, ctst_node_ref left, ctst_node_ref right, unsigned char* bytes, int bytes_index, int bytes_length);
-void ctst_storage_free(ctst_node_ref node);
+typedef struct struct_ctst_storage {
+  unsigned int n_blocks;
+  char** blocks;
+} ctst_storage;
+
+typedef struct struct_ctst_tworefs {
+  ctst_node_ref ref1;
+  ctst_node_ref ref2;
+} ctst_tworefs;
+
+/* Storage allocation / deallocation */
+ctst_storage* ctst_storage_alloc();
+void ctst_storage_free(ctst_storage* storage);
+
+/* Node allocation / deallocation */
+ctst_node_ref ctst_storage_node_alloc(ctst_storage* storage, ctst_data data, ctst_node_ref next, ctst_node_ref left, ctst_node_ref right, char* bytes, size_t bytes_index, size_t bytes_length);
+void ctst_storage_node_free(ctst_storage* storage, ctst_node_ref node);
+
+/* Node attribute reading */
+ctst_data ctst_storage_get_data(ctst_storage* storage, ctst_node_ref node);
+ctst_node_ref ctst_storage_get_next(ctst_storage* storage, ctst_node_ref node);
+ctst_node_ref ctst_storage_get_left(ctst_storage* storage, ctst_node_ref node);
+ctst_node_ref ctst_storage_get_right(ctst_storage* storage, ctst_node_ref node);
+size_t ctst_storage_get_bytes_length(ctst_storage* storage, ctst_node_ref node);
+char* ctst_storage_get_bytes(ctst_storage* storage, ctst_node_ref node);
+
+/* Node attribute writing */
+ctst_node_ref ctst_storage_set_data(ctst_storage* storage, ctst_node_ref node, ctst_data data);
+ctst_node_ref ctst_storage_set_next(ctst_storage* storage, ctst_node_ref node, ctst_node_ref next);
+ctst_node_ref ctst_storage_set_left(ctst_storage* storage, ctst_node_ref node, ctst_node_ref left);
+ctst_node_ref ctst_storage_set_right(ctst_storage* storage, ctst_node_ref node, ctst_node_ref right);
+ctst_node_ref ctst_storage_set_bytes(ctst_storage* storage, ctst_node_ref node, char* bytes, size_t bytes_index, size_t bytes_length);
+
+/* Special node operations */
+ctst_tworefs ctst_storage_swap_bytes(ctst_storage* storage, ctst_node_ref node1, ctst_node_ref node2);
+void ctst_storage_swap_last_byte(ctst_storage* storage, ctst_node_ref node1, ctst_node_ref node2);
+ctst_tworefs ctst_storage_split_node(ctst_storage* storage, ctst_node_ref node1, ctst_node_ref node2);
 
 #endif
