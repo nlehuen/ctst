888620ffdcb5364a61254d02e770e11838b55fc1
Nicolas Lehuen <nicolas@lehuen.com>
2007-11-17 12:55:08 +0000

    Skeleton of Ruby bindings, continued.

--- bindings/ruby/RTST.c
+++ bindings/ruby/RTST.c
@@ -2,13 +2,42 @@
 #include "ruby.h"
 #include <stdlib.h>
 
-static VALUE method_initialize(VALUE self) {
-  return self;
+typedef struct struct_rtst_ctst {
+  char* text;
+} rtst_ctst;
+
+static VALUE rtst_new(VALUE self) {
+  rtst_ctst *ctst;
+
+  VALUE obj = Data_Make_Struct(
+    self, // target object
+    rtst_ctst, // structure type
+    NULL, // mark function
+    free, // free function
+    ctst  // structure instance
+  );
+
+  ctst->text = "Hello, world";
+
+  return obj;
+}
+
+static VALUE rtst_to_s(VALUE self) {
+  rtst_ctst *ctst;
+
+  Data_Get_Struct(
+    self,
+    rtst_ctst,
+    ctst
+  );
+  
+  return rb_str_new2(ctst->text);
 }
 
 // The initialization method for this module
 void Init_rtst() {
 	VALUE RTST = rb_define_class("RTST",rb_cObject);
-	rb_define_method(RTST, "initialize", method_initialize, 0);
+	rb_define_singleton_method(RTST, "new", rtst_new, 0);
+	rb_define_method(RTST, "to_s", rtst_to_s, 0);
 }
 
