8c75b9d94b8d99ed2b462f84560814b4b30f3521
Nicolas Lehuen <nicolas@lehuen.com>
2007-11-17 13:08:45 +0000

    Better skeleton for bindings. Now I'll have to fight to handle includes and linking with the ctst sources.

--- bindings/ruby/RTST.c
+++ bindings/ruby/RTST.c
@@ -1,4 +1,3 @@
-// Include the Ruby headers and goodies
 #include "ruby.h"
 #include <stdlib.h>
 
@@ -6,18 +5,18 @@ typedef struct struct_rtst_ctst {
   char* text;
 } rtst_ctst;
 
-static VALUE rtst_new(VALUE self) {
-  rtst_ctst *ctst;
+static VALUE rtst_new(VALUE self,VALUE string) {
+  rtst_ctst *ctst = (rtst_ctst*)malloc(sizeof(rtst_ctst));
 
   VALUE obj = Data_Make_Struct(
-    self, // target object
-    rtst_ctst, // structure type
-    NULL, // mark function
-    free, // free function
-    ctst  // structure instance
+    self, /* target object */
+    rtst_ctst, /* structure type */
+    NULL, /* mark function */
+    free, /* free function */
+    ctst  /* structure instance */
   );
 
-  ctst->text = "Hello, world";
+  ctst->text = StringValuePtr(string);
 
   return obj;
 }
@@ -34,10 +33,12 @@ static VALUE rtst_to_s(VALUE self) {
   return rb_str_new2(ctst->text);
 }
 
-// The initialization method for this module
+/*
+ * The initialization method for this module
+ */ 
 void Init_rtst() {
 	VALUE RTST = rb_define_class("RTST",rb_cObject);
-	rb_define_singleton_method(RTST, "new", rtst_new, 0);
+	rb_define_singleton_method(RTST, "new", rtst_new, 1);
 	rb_define_method(RTST, "to_s", rtst_to_s, 0);
 }
 
