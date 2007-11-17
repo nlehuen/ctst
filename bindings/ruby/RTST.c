c9fe9cda29bbe0a057c672121f1c60fe99106f40
Nicolas Lehuen <nicolas@lehuen.com>
2007-11-17 14:02:07 +0000

    Began wrapping ctst, with get and set. I haven't found out how to include another source directory, though, so for the moment I manually copy extconf.rb and RTST.c inside the src directory and buid from there. It's ugly but I'd rather not fight with mkmf at this point.

--- bindings/ruby/RTST.c
+++ bindings/ruby/RTST.c
@@ -1,36 +1,55 @@
 #include "ruby.h"
+#include "include/ctst.h"
 #include <stdlib.h>
 
-typedef struct struct_rtst_ctst {
-  char* text;
-} rtst_ctst;
+static VALUE rtst_new(VALUE self) {
+  ctst_ctst *ctst = ctst_alloc();
 
-static VALUE rtst_new(VALUE self,VALUE string) {
-  rtst_ctst *ctst = (rtst_ctst*)malloc(sizeof(rtst_ctst));
-
-  VALUE obj = Data_Make_Struct(
+  VALUE obj = Data_Wrap_Struct(
     self, /* target object */
-    rtst_ctst, /* structure type */
     NULL, /* mark function */
-    free, /* free function */
+    ctst_free, /* free function */
     ctst  /* structure instance */
   );
-
-  ctst->text = StringValuePtr(string);
 
   return obj;
 }
 
-static VALUE rtst_to_s(VALUE self) {
-  rtst_ctst *ctst;
+static VALUE rtst_set(VALUE self, VALUE key, VALUE value) {
+  ctst_ctst *ctst;
 
   Data_Get_Struct(
     self,
-    rtst_ctst,
+    ctst_ctst,
     ctst
   );
   
-  return rb_str_new2(ctst->text);
+  ctst_data result = ctst_set(ctst, RSTRING(key)->ptr, 0, RSTRING(key)->len, value);
+  
+  if(result==0) {
+    return Qnil;
+  }
+  else {
+    return result;
+  }
+}
+
+static VALUE rtst_get(VALUE self, VALUE key) {
+  ctst_ctst *ctst;
+
+  Data_Get_Struct(
+    self,
+    ctst_ctst,
+    ctst
+  );
+  
+  ctst_data result = ctst_get(ctst, RSTRING(key)->ptr, 0, RSTRING(key)->len);
+  if(result==0) {
+    return Qnil;
+  }
+  else {
+    return result;
+  }
 }
 
 /*
@@ -38,7 +57,8 @@ static VALUE rtst_to_s(VALUE self) {
  */ 
 void Init_rtst() {
 	VALUE RTST = rb_define_class("RTST",rb_cObject);
-	rb_define_singleton_method(RTST, "new", rtst_new, 1);
-	rb_define_method(RTST, "to_s", rtst_to_s, 0);
+	rb_define_singleton_method(RTST, "new", rtst_new, 0);
+	rb_define_method(RTST, "get", rtst_get, 1);
+	rb_define_method(RTST, "set", rtst_set, 2);
 }
 
